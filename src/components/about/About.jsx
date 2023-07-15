4318f16bae045c264975ec83c8c9b07356c21b77
Djibh <julien.riera@protonmail.com>
2023-07-15 13:36:43 +0200

    feat(about): put datasource in dedicated file

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,24 +1,8 @@
 import homebannerImageSource from '../../assets/about-banner-img.png'
 import Banner from "../Banner"
 import Collapser from "../Collapser"
+import { listOfContent } from '../../datas/aboutPageContents'
 
-const content1 = {
-    'title': 'Fiabilité',
-    'content': 'Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.'
-}
-const content2 = {
-    'title': 'Respect',
-    'content': 'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.'
-}
-const content3 = {
-    'title': 'Service',
-    'content': 'Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N\'hésitez pas à nous contacter si vous avez la moindre question.'
-}
-const content4 = {
-    'title': 'Sécurité',
-    'content': 'La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l\'hôte qu\'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.'
-}
-const listOfContent = [content1, content2, content3, content4]
 
 function About() {
   return <>
@@ -26,7 +10,7 @@ function About() {
             <section>
                 {
                     listOfContent.map((content) =>
-                    <Collapser title={content.title} content={ content.content }/>)
+                    <Collapser title={ content.title } content={ content.content }/>)
                 }
             </section>
         </>
