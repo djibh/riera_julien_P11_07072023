b46e1627c79ea1068cf995c5e0211b45b1a3c367
Djibh <julien.riera@protonmail.com>
2023-07-15 13:33:57 +0200

    feat(about): add content for collapsers

--- src/components/Collapser.jsx
+++ src/components/Collapser.jsx
@@ -1,8 +1,16 @@
-function Collapser() {
+import '../styles/Collapser.css'
+
+function Collapser( {title, content} ) {
   return (
-    <article>
-      
-    </article>
+    <article className="details__description collapser">
+        <div className="collapser__title">
+          <span>{ title }</span>
+          <i className="fa-solid fa-chevron-up"></i>
+        </div>
+        <p className="collapser__content">
+        { content }
+        </p>
+      </article>
   )
 }
 
--- src/components/apartment/ApartmentDetails.jsx
+++ src/components/apartment/ApartmentDetails.jsx
@@ -6,7 +6,7 @@ function ApartmentDetails() {
       <article className="details__description collapser">
         <div className="collapser__title">
           <span>Description</span>
-          <i class="fa-solid fa-chevron-up"></i>
+          <i className="fa-solid fa-chevron-up"></i>
         </div>
         <p className="collapser__content">
         Vous serez à 50m du canal Saint-martin où vous pourrez pique-niquer l'été et à côté de nombreux bars et restaurants. Au cœur de Paris avec 5 lignes de métro et de nombreux bus. Logement parfait pour les voyageurs en solo et les voyageurs d'affaires. Vous êtes à1 station de la gare de l'est (7 minutes à pied).
@@ -15,7 +15,7 @@ function ApartmentDetails() {
       <article className="details__equipement collapser">
         <div className="collapser__title">
           <span>Equipement</span>
-          <i class="fa-solid fa-chevron-up"></i>
+          <i className="fa-solid fa-chevron-up"></i>
         </div>
         <ul className="collapser__content">
           <li>Climatisation</li>
