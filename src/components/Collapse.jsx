7f1ecdd84cb46a4e33aa3b61876b8a53eabd2ebb
Djibh <julien.riera@protonmail.com>
2023-07-22 16:39:50 +0200

    fix(Collapse): remove bug in apartment details + update global padding for responsiveness

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -6,12 +6,14 @@ function Collapse( {title, content} ) {
   const handleClick = () => { setIsVisible(!isVisible) }
   
   return (
-    <article className="details__description collapser">
-        <div className="collapser__title" onClick={ handleClick }>
-          <span>{ title }</span>
-          <i className={isVisible ? "fa-solid fa-chevron-down active" : "fa-solid fa-chevron-down" }></i>
+    <article>
+      <div className="collapser">
+          <div className="collapser__title" onClick={ handleClick }>
+            <span>{ title }</span>
+            <i className={isVisible ? "fa-solid fa-chevron-down active" : "fa-solid fa-chevron-down" }></i>
+          </div>
+          <div className={isVisible ? "collapser__content" : "collapser__content hidden" }>{ content }</div> 
         </div>
-        <div className={isVisible ? "collapser__content" : "collapser__content hidden" }>{ content }</div> 
       </article>
   )
 }
